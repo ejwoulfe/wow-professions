f0d55a1eb6450dd54b194fde391761bef0796057
Ed <edwoulfe@gmail.com>
2020-03-12 11:30:44 -0500

    Was able to iterate through all of the table rows getting the recipe link for each. It will check if there is a next button available, if so it will go to the next page and fill the array. If not it will end.

--- web_scrape/scrape_recipes.js
+++ web_scrape/scrape_recipes.js
@@ -4,47 +4,50 @@ const puppeteer = require('puppeteer');
 (async () => {
 
 
+
     const browser = await puppeteer.launch({
-        headless: true
+        headless: false
     });
     const page = await browser.newPage();
     await page.setViewport({
         width: 1200,
-        height: 1200
+        height: 1800
     })
     await page.goto('https://www.wowhead.com/alchemy-recipe-items?filter=166;8;0#0+18', {
         waitUntil: 'domcontentloaded'
     });
     await page.waitFor(4000);
-    const data = await page.evaluate(() => {
+    const getRecipes = await page.evaluate(() => {
         let recipesArray = [];
         let nextButton = document.querySelector("#lv-items > div.listview-band-bottom > div.listview-nav > a:nth-child(4)");
 
-
-
-        function getRecipeLink(array) {
+        function fillArray(array) {
             let table = Array.from(document.querySelectorAll('table.listview-mode-default tbody tr td div.iconmedium a'));
-            array.push(table.map(link => {
-                return link.getAttribute("href");
-            }))
+            array.push(table.map(link =>
+                link.getAttribute("href")
+            ));
         }
-        //getRecipeLink(recipesArray);
-
-        if (window.getComputedStyle(nextButton).display === "none") {
-            getRecipeLink(recipesArray);
-            return recipesArray;
-        } else {
-            getRecipeLink(recipesArray);
-            await page.click(nextButton);
-
+        while (window.getComputedStyle(nextButton).display !== "none") {
+            fillArray(recipesArray);
+            nextButton.click();
         }
+        fillArray(recipesArray);
+
+
+        return recipesArray;
+
+    })
+
+
+    let merged = [].concat.apply([], getRecipes);
+    console.log(merged.length)
+    let url = 'https://www.wowhead.com' + merged[0];
 
 
 
-
+    await page.goto(url, {
+        waitUntil: 'domcontentloaded'
     });
-    console.log(data)
-
-
+    await page.waitFor(2000);
     await browser.close();
 })();
